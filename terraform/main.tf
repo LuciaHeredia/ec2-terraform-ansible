0b3e54c3da8c37ea0c5555822533212f1fee260a
Lucia Heredia <luciaheredia22@gmail.com>
2025-05-15 20:27:07 +0300

    terraform-ec2 done

---
+++ terraform/main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_key_pair" "deployer" {
+  # if u dont have, run: ssh-keygen -t rsa -b 4096 -f ~/.ssh/deployer-key
+  # then allow permission: chmod 400 ~/.ssh/deployer-key
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "ssh" {
+  # using default VPC
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # for testing
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  count         = var.num_ec2
+  ami           = var.ami_id
+  instance_type = var.type_ec2
+  key_name      = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.ssh.id]
+
+  tags = {
+    Name = "web-${count.index + 1}"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Hello from instance ${self.tags["Name"]}'"]
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.public_ip
+      timeout     = "2m"
+    }
+  }
+}
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,4 +1,4 @@
 aws_region = "us-east-2"
-ami_id = "ami-0c94855ba95c71c99"
+ami_id = "ami-0884d2865dbe9de4b" # ubuntu 22.04
 num_ec2 = 3
 type_ec2 = "t2.micro"
